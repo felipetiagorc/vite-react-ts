3d3b5fd179be816a683568496eff84ddfcc0ebdd
felipe <felipetiagorc@yahoo.com.br>
2021-12-14 02:18:49 -0300

    login

---
+++ src/context/AuthProvider/index.tsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState } from 'react'
+import { IAuthProvider, IContext, IUser } from './types'
+
+// cria o contexto pra dar e receber valores
+export const AuthContext = createContext<IContext>({} as IContext)
+
+// esse fica por volta e controla o estado do login/logout
+//recebe props.children do tipo IAuthProvider
+export const AuthProvider = ({children}: IAuthProvider) =>{
+    const [user, setUser] = useState<IUser | null>()
+
+    async function authenticate(email: string, password: string){
+    }
+    function logout() {}
+
+    return (
+        <AuthContext.Provider value={{...user, authenticate, logout }}> 
+            {children}
+        </AuthContext.Provider>
+    )
+    // 20 mint video
+    // https://www.youtube.com/watch?v=kPtTguJlRFY&list=PL6FcAU2NuI3UyCEmdYfhoB9Bz1ahGmcEA&index=3&t=7s
+}
